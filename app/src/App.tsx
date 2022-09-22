e65151587e7a37cd27298b07243cd12a40c6deb3
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-09-22 11:20:06 +0600

    Added frontend

---
+++ app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+        Hello
+        <button>Increment</button>
+        <button>Decrement</button>
+        <button>Set</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
