215ad4fe910358c2c998da4318c10e16721e038f
ShahEftakher <shaheftakher@iut-dhaka.edu>
2022-09-27 16:19:11 +0600

    changed to js as ts-node showing error

--- copyIDL.js
+++ copyIDL.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const idl = require('./target/idl/anchor_counter.json');
 
-fs.writeFileSync('./app/src/idl.json', JSON.stringify(idl));
+fs.writeFileSync('./app/public/idl.json', JSON.stringify(idl));
